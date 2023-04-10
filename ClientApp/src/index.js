43283512bca3e909422c26f0a04753e09b6db073
Jose Guerrero <joseguerrero@local>
2023-04-10 08:17:42 -0400

    fix issue with routing

--- ClientApp/src/index.js
+++ ClientApp/src/index.js
@@ -1,19 +1,18 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <BrowserRouter basename={baseUrl}>
+  <HashRouter>
     <App />
-  </BrowserRouter>);
+  </HashRouter>);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
